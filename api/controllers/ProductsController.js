1c4a1595cbe2666b89c97d2209c291ddd0d7ce10
Ahmad Elgabri <ahmedlg46@gmail.com>
2019-10-16 23:34:00 +0200

    first commit

---
+++ api/controllers/ProductsController.js
@@ -0,0 +1,14 @@
+/**
+ * ProductsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  fetchproducts: function(req, res) {
+    // res.send('yolo');
+  }
+
+};
+
---
+++ api/models/Products.js
@@ -0,0 +1,59 @@
+/**
+ * Products.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  primaryKey: 'id',
+  attributes: {
+    id: {
+      type : 'integer',
+      unique : true,
+      autoIncrement : true
+    },
+    nameEnglish: {
+      type: 'string',
+      required: true
+    },
+    nameArabic: {
+      type: 'string',
+    },
+    descriptionEnglish: {
+      type: 'string'
+    },
+    descriptionArabic: {
+      type: 'string'
+    },
+    price: {
+      type: 'number',
+      required: true
+    },
+    quantity: {
+      type: 'number',
+      required: true
+    },
+    images: {
+      type: 'json',
+      columnType: 'array'
+    }
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+  datastore: 'default'
+};
+
