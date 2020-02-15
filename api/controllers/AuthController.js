114cb93b14b78e85ddffb36c8addd03b31a82f74
Ahmad Elgabri <ahmedlg46@gmail.com>
2020-02-15 16:06:48 +0200

    production database

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -27,6 +27,12 @@ module.exports = {
         message: 'Missing body',
       });
     }
+    const oldUSers = await Auth.find({email: req.body.email});
+    if(oldUSers.length > 0) {
+      return res.status(400).json({
+        message: 'Duplicate EMail',
+      });
+    }
     Auth.create(req.body.auth)
       .fetch()
       .then(user => {
